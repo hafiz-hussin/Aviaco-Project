ce6dc322ca5d26f756919375ecb9dc98e4f60274
Izzat Amal <izzat.amal@terang.asia>
2019-08-24 21:00:21 +0800

    asda

--- server.R
+++ server.R
@@ -2,8 +2,6 @@ library(shinydashboard)
 library(leaflet)
 library(leaflet.extras)
 library(dplyr)
-library(rjson)
-library(stringr)
 
 # data --------------------------------------------------------------------
 
@@ -11,29 +9,20 @@ function(input, output, session) {
   
   output$crimeMap <- renderLeaflet({
     year_selected <- input$slider
-    df <- fromJSON(file = "airport.json")
-    json_data_frame <- as.data.frame(df)
-    
-    coor <- str_split(json_data_frame$groundSupportEquipments.Lifts.ScissorsLift001.current_coordinates, ',', n = 1, simplify = FALSE)
-    x <- coor[1]
-    y <- coor[2]
-    
-    freq_table <- df
-    #   select(X, Y, year) %>%
-    #   filter(year == year_selected) %>% 
-    #   group_by(X, Y) %>% 
-    #   summarise(Total = n())
+    df <- read.csv("db/equipments.csv")
+    freq_table <- df %>% 
+      select(X, Y, year) %>%
+      filter(year == year_selected) %>% 
+      group_by(X, Y) %>% 
+      summarise(Total = n())
     
     map <- leaflet(freq_table) %>% 
       addTiles(
         urlTemplate = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>% 
-      setView(lng = 101.685149, lat = 2.740894, zoom = 17) %>% 
-      addAwesomeMarkers(as.numeric(x),
-                        as.numeric(y),
-                        popup = paste(sep="div",
-                                      leafpop::popupImage(src="logo.png")))
+      addHeatmap(lng = ~X, lat = ~Y, radius = 8) %>% 
+      setView(lng = 101.685149, lat = 2.740894, zoom = 17)
     map
   })
 }
