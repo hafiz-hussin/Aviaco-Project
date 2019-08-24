29df19e0c5b835b5cceb685b32d09d8bdd8e2852
Nate C <N_t_N@live.com>
2019-08-25 01:18:46 +0800

    presets

--- server.R
+++ server.R
@@ -21,10 +21,11 @@ function(input, output, session) {
     df <- fromJSON(file = "airport.json")
     json_data_frame <- as.data.frame(df)
     equipment_df <- read.csv("db/equipments.csv")
-    
-    coor <- str_split(json_data_frame$groundSupportEquipments.Lifts.ScissorsLift001.current_coordinates, ',', n = 1, simplify = FALSE)
-    x <- coor[1]
-    y <- coor[2]
+
+    airportIcons <- iconList(
+      aircraft = makeIcon("icon/g-plane.svg", "icon/plane.svg", 18, 18),
+      towbar = makeIcon("icon/g-towbar.png", "icon/towbar.png", 24, 24)
+    )
     
     freq_table <- df
     #   select(X, Y, year) %>%
@@ -38,21 +39,33 @@ function(input, output, session) {
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>% 
       setView(lng = 101.685149, lat = 2.740894, zoom = 17) %>% 
-      addAwesomeMarkers(as.numeric(equipment_df$x),
-                        as.numeric(equipment_df$y),
-                        popup = ~paste0("<br/>Equipment: ", equipment_df$equipments, 
-                                        "<br/>State: ", equipment_df$state,
-                                        "<br/>Ground: ", equipment_df$belongsTo,
-                                        "<br/>Status: ", equipment_df$status,
-                                        "<br/>Current User: ", equipment_df$current_user,
-                                        "<br/>Previous User: ", equipment_df$previous_user,
-                                        "<br/>Description: ", equipment_df$description,
-                                        "<br/>Next Service Date: ", equipment_df$nextMaintenanceDate,
-                                        "<br/>Previous Service Date: ", equipment_df$previousMaintenanceDate
-                                        )
-                        # popup = paste(sep="div",
-                        #               leafpop::popupImage(as.character(equipment_df$image_src)))
-                        )
+      addMarkers(as.numeric(equipment_df$x),
+                 as.numeric(equipment_df$y),
+                 popup = ~paste0("<br/>Equipment: ", equipment_df$id, 
+                                 "<br/>State: ", equipment_df$state,
+                                 "<br/>Ground: ", equipment_df$belongsTo,
+                                 "<br/>Status: ", equipment_df$status,
+                                 "<br/>Current User: ", equipment_df$current_user,
+                                 "<br/>Previous User: ", equipment_df$previous_user,
+                                 "<br/>Description: ", equipment_df$description,
+                                 "<br/>Next Service Date: ", equipment_df$nextMaintenanceDate,
+                                 "<br/>Previous Service Date: ", equipment_df$previousMaintenanceDate)
+                 , icon = ifelse(as.numeric(equipment_df$equipments) == 1, airportIcons["towbar"], airportIcons["aircraft"]))
+      # addAwesomeMarkers(as.numeric(equipment_df$x),
+      #                   as.numeric(equipment_df$y),
+      #                   popup = ~paste0("<br/>Equipment: ", equipment_df$equipments, 
+      #                                   "<br/>State: ", equipment_df$state,
+      #                                   "<br/>Ground: ", equipment_df$belongsTo,
+      #                                   "<br/>Status: ", equipment_df$status,
+      #                                   "<br/>Current User: ", equipment_df$current_user,
+      #                                   "<br/>Previous User: ", equipment_df$previous_user,
+      #                                   "<br/>Description: ", equipment_df$description,
+      #                                   "<br/>Next Service Date: ", equipment_df$nextMaintenanceDate,
+      #                                   "<br/>Previous Service Date: ", equipment_df$previousMaintenanceDate
+      #                                   )
+      #                   # popup = paste(sep="div",
+      #                   #               leafpop::popupImage(as.character(equipment_df$image_src)))
+      #                   )
     map
   })
   
